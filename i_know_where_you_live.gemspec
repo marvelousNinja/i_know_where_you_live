2832a65c78ef11cc8406bd51b2da124b218a1afa
Alexander Sologub <alexsologoub@gmail.com>
2013-09-04 15:40:19 +0300

    Hooks: Lets simplify it a bit

--- i_know_where_you_live.gemspec
+++ i_know_where_you_live.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency 'rails', '~> 4.0.0'
   s.add_dependency 'nokogiri', '~> 1.6.0'
-  
+
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'pry-nav'
   s.add_development_dependency 'rspec-rails'
--- lib/i_know_where_you_live/hook_definitions.rb
+++ lib/i_know_where_you_live/hook_definitions.rb
@@ -1,28 +1,21 @@
 module IKnowWhereYouLive
 	module RenderHooks
-		extend ActiveSupport::Concern
-		
-		included do
-		  alias_method :original_render_template, :render_template
-		  alias_method :original_render_partial, :render_partial
+		def render_template(context, options)
+			output = super
+			after_render_template(output, context, options)
+		end
 
-		  def render_template(context, options)
-		  	output = original_render_template(context, options)
-		  	after_render_template(output, context, options)
-		  end
+		def render_partial(context, options, &block)
+			output = super
+			after_render_partial(context, options, block)
+		end
 
-		  def render_partial(context, options, &block)
-        output = original_render_partial(context, options, block)
-        after_partial_render(output, context, options, block)
-		  end
+		def after_render_template(output, context, options)
+			raise NotImplementedError
+		end
 
-		  def after_render_template(output, context, options)
-		  	raise NotImplementedError
-		  end
-
-		  def after_render_partial(output, context, options, &block)
-		  	raise NotImplementedError
-		  end
+		def after_render_partial(output, context, options, &block)
+		  raise NotImplementedError
 		end
 	end
 end
