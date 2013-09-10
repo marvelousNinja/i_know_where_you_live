bfebdf806918d0c7e999408d9cd97a20c4c6ecfa
Alexander Sologub <alexsologoub@gmail.com>
2013-09-10 20:40:44 +0300

    Highlighting: Rough implementation

--- app/assets/javascripts/i_know_where_you_live.js
+++ app/assets/javascripts/i_know_where_you_live.js
@@ -1,1 +1,18 @@
-alert('hello');
+(function() {
+	path_attribute_name = 'data-rails-path';
+
+  marked_elements = document.querySelectorAll('[' + path_attribute_name + ']');
+  elements_count = marked_elements.length;
+  color_by_path = {}
+
+  for (var i = 0; i < elements_count; i++ ) {
+  	element = marked_elements[i];
+  	path = element.getAttribute(path_attribute_name);
+
+  	if (!(color_by_path[path])) {
+  		color_by_path[path] = "#"+((1<<24)*Math.random()|0).toString(16);
+  	}
+
+  	element.style.background = color_by_path[path];
+  }
+})();
